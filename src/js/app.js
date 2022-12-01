ddd4668b4f5fd1745a3814ea6e45686e490c12c1
tanyavulcheva23 <tantellka07@gmail.com>
2022-12-01 17:43:52 +0200

    task

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,13 @@ window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
+    .then(response => response.json())
+    .then(data => {
+        for (let i=0; i < data.results.length; i++){
+            const li = document.createElement("li")
+            li.innerText = data.results[i].name;
+            ul.appendChild(li);
+        }
+    })
 });
